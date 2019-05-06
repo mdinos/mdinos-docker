d1085109d0b9edc0042fa5788d4dc80043b71e9f
mdinos <32902031+mdinos@users.noreply.github.com>
2019-05-06 02:45:53 +0100

    Host on port 80

--- rs-api/rs_api.py
+++ rs-api/rs_api.py
@@ -47,4 +47,4 @@ def not_found(error='Something went wrong.'):
 	return make_response(jsonify({'error': error}), 404)
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0', port=80)
